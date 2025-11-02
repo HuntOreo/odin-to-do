8bb752d14aa0b379d84132a41c3658805fd8171e
HuntOreo <106197781+HuntOreo@users.noreply.github.com>
2025-11-02 18:19:40 +0000

    Render priority tasks on page load

--- src/Helpers/events/updatePriority.js
+++ src/Helpers/events/updatePriority.js
@@ -2,6 +2,7 @@ import { updateCookie } from "../handleCookies";
 
 const updatePriority = (e, task, taskList) => {
     task.priority = e.target.checked;
+    console.log(task);
     const index = taskList.findIndex(child => child.id === task.id);
     taskList[index] = task;
 
